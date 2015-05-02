01f7d00883541ed9b9f5bbd2b6216a667efeb9ef
ViaSyN <viasyn@yandex-team.ru>
2015-05-02 18:07:14 +0600

    errors history

--- stplot.py
+++ stplot.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import zipfile
 import shutil
 import os
+import sys
 import logging
 from datetime import datetime
 
@@ -23,8 +24,14 @@ def save_figure(output_directory, format, *args):
     name = format % args
     fname = os.path.join(output_directory, name)
     plt.savefig(fname)
-    return '<p><img src="%s" alt="%s"/></p>' % (name, name)
+    return '<img src="%s" alt="%s"/>' % (name, name)
 
+class Test(object):
+    def __init__(self, hostname, testname, success_history, errors):
+        self.hostname = hostname
+        self.testname = testname
+        self.success_history = success_history
+        self.errors = errors
 
 def process_zip_report(fname, output_directory):
     zip = zipfile.ZipFile(fname)
@@ -42,16 +49,29 @@ def process_zip_report(fname, output_directory):
             hostname = parts[0]
             testname = os.path.splitext(parts[2])[0]
 
-            content = zip.open(n, 'r')
-            result = np.genfromtxt(content, delimiter=";")
+            with zip.open(n, 'r') as content:
+                result = np.genfromtxt(content, delimiter=";")
+                history = np.delete(result, [0,1,3,5], 1)
+                history = np.sort(history, axis=0)
 
-            history = np.delete(result, [0,1,3,5], 1)
-            history = np.sort(history, axis=0)
-            success_history.append((hostname, testname, history))
+                err_parts = parts
+                err_parts[1] = "calls_failed"
+                errs = None
+                errname = os.sep.join(err_parts)
+                if errname in names:
+                    with zip.open(errname) as err_content:
+                        err_result = np.genfromtxt(err_content, delimiter=";")
+                        errs = np.delete(err_result, [0,1,3,5,6], 1)
+                        errs = np.sort(errs, axis=0)
+                success_history.append(Test(hostname, testname, history, errs))
 
     html = "<h2>%s</h2>" % zipname
 
-    for (hostname, testname, history) in success_history:
+    for test in success_history:
+            hostname = test.hostname
+            testname = test.testname
+            history = test.success_history
+
             timestamps_nanos = history[:, 0]
             result_times = history[:, 1]
 
@@ -60,19 +80,27 @@ def process_zip_report(fname, output_directory):
             #to_ts = np.vectorize(nanos_to_ts)
             timestamps_msec = np.apply_along_axis(lambda n: nanos_to_millis(n - timestamps_nanos[0]), 0, timestamps_nanos)
 
+            err_timestamps_msec = None
+            if test.errors is not None:
+                print(testname, test.errors)
+                err_nanos = test.errors[:, 0]
+                err_timestamps_msec = np.apply_along_axis(lambda n: nanos_to_millis(n - err_nanos[0]), 0, err_nanos)
+
             hist = sorted(result_msec)
-            fit = stats.norm.pdf(hist, np.mean(hist), np.std(hist))
+            #fit = stats.norm.pdf(hist, np.mean(hist), np.std(hist))
 
             html += "<h3>%s at %s</h3>" % (testname, hostname)
             plt.close()
             weights = np.ones_like(hist) / len(hist)
             plt.hist(hist,  histtype='stepfilled', normed=0, weights=weights, cumulative=True, color='b', label='Cumulative probability')
             plt.hist(hist,  histtype='stepfilled', normed=0, weights=weights, color='r', alpha=0.5, label='Probability')
-            plt.plot(hist, fit, '-o', color='y')
+            #plt.plot(hist, fit, color='y-o')
+            plt.plot(hist, stats.norm.pdf(hist), 'y-', lw=2, label='pdf')
             plt.title("Normalized distribution of '%s' on %s" % (testname, hostname))
             plt.xlabel("Action time, msec")
             plt.ylabel("Probability")
             plt.legend()
+            html += "<p>"
             html += save_figure(output_directory, "%s_%s_dist_normed.png", hostname, testname)
 
             plt.close()
@@ -82,6 +110,7 @@ def process_zip_report(fname, output_directory):
             plt.ylabel("Frequency")
             plt.legend()
             html += save_figure(output_directory, "%s_%s_dist_freq.png", hostname, testname)
+            html += "</p>"
 
             plt.close()
             plt.plot(timestamps_msec, result_msec, 'g-^')
@@ -90,7 +119,19 @@ def process_zip_report(fname, output_directory):
             plt.xlabel("Time since test start, msec")
             plt.ylabel("Action time, msec")
             plt.legend()
+            html += "<p>"
             html += save_figure(output_directory, "%s_%s_history.png", hostname, testname)
+
+            if err_timestamps_msec is not None:
+                plt.close()
+                plt.hist(err_timestamps_msec, bins=30, histtype='stepfilled', normed=False, color='r', label='Errors (total %d)' % len(err_timestamps_msec))
+                plt.title("Error history of '%s' on %s" % (testname, hostname))
+                plt.xlabel("Time since test start, msec")
+                plt.ylabel("Errors count")
+                plt.legend()
+                html += save_figure(output_directory, "%s_%s_err_history.png", hostname, testname)
+
+            html += "</p>"
     return html
 
 
@@ -105,7 +146,7 @@ def setup_logging():
 
 
 if __name__ == '__main__':
-    fname = "sample/stresstest-latitude-rw-2015-121-05--11-49-47.zip"
+    fnames = set(["sample/stresstest-latitude-rw-2015-121-05--11-49-47.zip"])
     output = "output"
 
     try:
@@ -120,4 +161,7 @@ if __name__ == '__main__':
     setup_logging()
 
     with open(os.path.join(output, "report.html"), "w+") as out:
-        out.write(process_zip_report(fname, output))
+        out.write("<html><body>")
+        for fname in fnames:
+            out.write(process_zip_report(fname, output))
+        out.write("</body></html>")
