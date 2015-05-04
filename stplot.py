8d4c7cda502c02f605e9e4b532cf2984e2205043
ViaSyN <viasyn@yandex-team.ru>
2015-05-04 16:01:20 +0600

    l&f improved

--- stplot.py
+++ stplot.py
@@ -12,8 +12,11 @@ import sys
 import logging
 from datetime import datetime
 import tempfile
+from jinja2 import *
 
 logger = logging.getLogger('stplot')
+
+FORMAT = "png"
 
 def nanos_to_millis(nanos):
     return nanos * 10 ** -6
@@ -21,16 +24,24 @@ def nanos_to_millis(nanos):
 def nanos_to_ts(nanos):
     return datetime.fromtimestamp(int(nanos) // 1000000000)
 
-def save_figure(output_directory, format, *args):
-    name = format % args
-    fname = os.path.join(output_directory, name)
-    plt.savefig(fname)
-    return '<img src="%s" alt="%s"/>' % (name, name)
+def save_figure(output_directory, fname):
+    targetsubdir = "graphs"
+    targetdir = os.path.join(output_directory, targetsubdir)
+
+    try:
+        os.makedirs(targetdir)
+    except:
+        pass
+
+    name = os.path.join(targetdir, fname)
+    plt.savefig(name, dpi=100)
+    return os.path.join(targetsubdir, fname)
 
 def zipdir(path, zip):
     for root, dirs, files in os.walk(path):
         for file in files:
-            zip.write(os.path.join(root, file), arcname=file)
+            print(root, dirs, file)
+            zip.write(os.path.join(root, file), arcname=os.path.join(os.path.relpath(root, path), file))
 
 
 class Test(object):
@@ -39,6 +50,26 @@ class Test(object):
         self.testname = testname
         self.success_history = success_history
         self.errors = errors
+        
+class TestSuiteRepr(object):
+    def __init__(self, zipname):
+        self.zipname = zipname
+        self.tests = []
+
+    def latest(self):
+        return self.tests[-1]
+        
+        
+class TestRepr(object):
+    def __init__(self, name, hostname):
+        self.name = name
+        self.hostname = hostname
+        self.dist = None
+        self.freq = None
+        self.errors = None
+        self.hist = None
+
+
 
 def process_zip_report(fname, output_directory):
     zip = zipfile.ZipFile(fname)
@@ -72,8 +103,7 @@ def process_zip_report(fname, output_directory):
                         errs = np.sort(errs, axis=0)
                 success_history.append(Test(hostname, testname, history, errs))
 
-    html = "<h2>%s</h2>" % zipname
-
+    suiteRepr = TestSuiteRepr(zipname)
     for test in success_history:
             hostname = test.hostname
             testname = test.testname
@@ -95,50 +125,49 @@ def process_zip_report(fname, output_directory):
             hist = sorted(result_msec)
             #fit = stats.norm.pdf(hist, np.mean(hist), np.std(hist))
 
-            html += "<h3>%s at %s</h3>" % (testname, hostname)
+            suiteRepr.tests.append(TestRepr(testname, hostname))
             plt.close()
             weights = np.ones_like(hist) / len(hist)
-            plt.hist(hist,  histtype='stepfilled', normed=0, weights=weights, cumulative=True, color='b', label='Cumulative probability')
-            plt.hist(hist,  histtype='stepfilled', normed=0, weights=weights, color='r', alpha=0.5, label='Probability')
+            plt.hist(hist,  histtype='bar', normed=0, weights=weights, cumulative=True, color="#6495ed", label='Cumulative probability')
+            plt.hist(hist,  histtype='step', normed=0, weights=weights, color='#00008b', lw=4, label='Probability')
             #plt.plot(hist, fit, color='y-o')
-            plt.plot(hist, stats.norm.pdf(hist), 'y-', lw=2, label='pdf')
+            plt.plot(hist, stats.norm.pdf(hist), '-', color="#b0e0e6", lw=4, label='pdf')
             plt.title("Normalized distribution of '%s' on %s" % (testname, hostname))
             plt.xlabel("Action time, msec")
             plt.ylabel("Probability")
             plt.legend()
-            html += "<p>"
-            html += save_figure(output_directory, "%s_%s_dist_normed.png", hostname, testname)
+            suiteRepr.latest().dist = save_figure(output_directory, "%s_%s_dist_normed.%s" % (hostname, testname, FORMAT))
 
             plt.close()
-            plt.hist(hist, bins=30, histtype='stepfilled', normed=False, color='b', label='Frequency')
+            plt.hist(hist, bins=30, histtype='bar', normed=False, color="#6495ed", label='Frequency')
             plt.title("Distribution of '%s' on %s" % (testname, hostname))
             plt.xlabel("Action time, msec")
             plt.ylabel("Frequency")
             plt.legend()
-            html += save_figure(output_directory, "%s_%s_dist_freq.png", hostname, testname)
-            html += "</p>"
+            suiteRepr.latest().freq = save_figure(output_directory, "%s_%s_dist_freq.%s" % (hostname, testname, FORMAT))
 
             plt.close()
-            plt.plot(timestamps_msec, result_msec, 'g-^')
-            plt.gcf().autofmt_xdate()
-            plt.title("History of '%s' on %s" % (testname, hostname))
-            plt.xlabel("Time since test start, msec")
-            plt.ylabel("Action time, msec")
-            plt.legend()
-            html += "<p>"
-            html += save_figure(output_directory, "%s_%s_history.png", hostname, testname)
+
+            fig, ax1 = plt.subplots()
+            ax2 = ax1
+            fig.set_size_inches(20, 5)
 
             if err_timestamps_msec is not None:
-                plt.close()
-                plt.hist(err_timestamps_msec, bins=err_timestamps_msec[-1] / 200, histtype='stepfilled', normed=False, color='r', label='Errors (total %d)' % len(err_timestamps_msec))
-                plt.title("Error history of '%s' on %s" % (testname, hostname))
-                plt.xlabel("Time since test start, msec")
-                plt.ylabel("Errors count")
-                plt.legend()
-                html += save_figure(output_directory, "%s_%s_err_history.png", hostname, testname)
+                ax1.hist(err_timestamps_msec, bins=err_timestamps_msec[-1] / 250, histtype='barstacked',
+                         color='#fa8072', label='Errors (total %d)' % len(err_timestamps_msec))
+                ax1.set_xlabel("Time since test start, msec")
+                ax1.set_ylabel("Errors count")
+                ax1.legend()
+                ax2 = ax1.twinx()
 
-            html += "</p>"
-    return html
+            ax2.plot(timestamps_msec, result_msec, '-o')
+            ax2.set_title("History of '%s' on %s" % (testname, hostname))
+            ax2.set_xlabel("Time since test start, msec")
+            ax2.set_ylabel("Action time, msec")
+
+            suiteRepr.latest().hist = save_figure(output_directory, "%s_%s_history.%s" % (hostname, testname, FORMAT))
+
+    return suiteRepr
 
 
 def setup_logging():
@@ -151,18 +180,37 @@ def setup_logging():
     logger.addHandler(ch)
 
 
-if __name__ == '__main__':
-    fnames = set(sys.argv[1:])
+def main(fnames):
     output = tempfile.mkdtemp("stplotter")
+    """
+    output = "stplotterout"
+    try:
+        shutil.rmtree(output)
+    except Exception:
+        pass
+    try:
+        os.makedirs(output)
+    except Exception:
+        pass
+    """
 
     setup_logging()
+    env = Environment(loader=PackageLoader('stemplates', '.'))
+    template = env.get_template('report.html')
+    suites = []
+
+    for fname in fnames:
+        logger.info("Working on %s" % fname)
+        suites.append(process_zip_report(fname, output))
 
     with open(os.path.join(output, "report.html"), "w+") as out:
-        out.write("<html><body>")
-        for fname in fnames:
-            out.write(process_zip_report(fname, output))
-        out.write("</body></html>")
+        out.write(template.render(suites=suites))
 
     zipf = zipfile.ZipFile('report-%s.zip' % (datetime.now().strftime("%Y-%m-%d--%H-%M-%S")), 'w')
     zipdir(output, zipf)
     zipf.close()
+
+
+if __name__ == '__main__':
+    fnames = set(sys.argv[1:])
+    main(fnames)
